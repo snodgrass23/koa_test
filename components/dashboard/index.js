812101ac5b488721b9466cd3807d18a17802c2a0
Jim Snodgrass <jim@skookum.com>
2014-03-11 09:31:09 -0400

    tweaking style architecture and adding in demo react script

--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -1,5 +1,8 @@
+var path = require('path');
+
 module.exports = function(app){
 
+  app.use(require('koa-static')(path.join(__dirname, 'public')));
   app.use(require('koa-views')(__dirname, 'jade', {}));
   app.use(require('koa-router')(app));
 
